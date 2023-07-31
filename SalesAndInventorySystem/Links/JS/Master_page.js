ad99af2ef415842f341335377d22da778ac5c078
pussit <zumorinkashi24@gmail.com>
2023-07-31 09:05:09 +0800

    NewUpdate

--- SalesAndInventorySystem/Links/JS/Master_page.js
+++ SalesAndInventorySystem/Links/JS/Master_page.js
@@ -1,27 +1,9 @@
-﻿$(() => {
-
-    $('.logo_con').click(() => {
-        $('.modal_login').addClass('show_modal_login')
-        $('.login').addClass('show_login')
-    })
-
-    $('.modal_login:not(> .login)').click(() => {
-        $('.modal_login').removeClass('show_modal_login')
-        $('.login').removeClass('show_login')
-
-    })
-})
-
-$(window).scroll(e => {
+﻿$(window).scroll(e => {
     let nav = $('.nav_con')
     let current_pos = $(window).scrollTop()
     let prev_pos = $('.nav_con').attr('data-position')
 
     $('.nav_con').attr('data-position', current_pos)
-
-
-    console.log('current_pos:', current_pos)
-    console.log('prev_pos:', prev_pos)
 
     if (current_pos > 150 && current_pos > prev_pos) {
         nav.addClass('nav_hide')
@@ -29,5 +11,27 @@ $(window).scroll(e => {
         nav.removeClass('nav_hide')
     }
 
+    if ($('.global_search:focus').length > 0) {
+        nav.removeClass('nav_hide')
+    }
+})
+
+$(() => {
+    $('.user_con').click(() => {
+        $('.modal_login').addClass('show_modal_login')
+        $('.login').addClass('show_login')
+    })
+
+    $('.modal_login').click(() => {
+        $('.modal_login').removeClass('show_modal_login')
+        $('.login').removeClass('show_login')
+    })
+
+    $('.login').click(e => e.stopPropagation())
+
+
+
+
 
 })
+
--- SalesAndInventorySystem/Pages/Home.aspx
+++ SalesAndInventorySystem/Pages/Home.aspx
@@ -6,6 +6,12 @@
 
 <%-- HTML / CONTENT --%>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        
+    <div class="">
+        asdsdasdasd
+    </div>
+
+
 </asp:Content>
 
 
